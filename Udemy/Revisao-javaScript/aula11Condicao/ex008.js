f71e8f01822f5b6ebcb3aab186d02813cb580844
thiago-igor <thiago.maia0606@gmail.com>
2023-07-13 09:31:34 -0300

    atualização

---
+++ Udemy/Revisao-javaScript/aula11Condicao/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5 
+console.log(`Sua velocidade é de ${vel} km/h`)
+
+if(vel > 60){
+    console.log('Você foi multado, Ande mais devagar!')
+}
+
+console.log('Dirija sempre com o cinto de segurança!!')
