8fbc2fc7d6f5ac56a53f36b4c738f2884d48d7cf
thiago-igor <thiago.maia0606@gmail.com>
2023-08-03 11:23:47 -0300

    atualização js

--- Udemy/javaScript-/Logica/aula66Cronometro/assets/js/main.js
+++ Udemy/javaScript-/Logica/aula66Cronometro/assets/js/main.js
@@ -1,28 +1,70 @@
-function criaHoraDosSegundos(segundos) {
-    const data = new Date(segundos)
-    return data.toLocaleTimeString('pt-BR',{
-        hour12: false,
-        timeZone: 'UTC' // pegando a data 01/01/1970 as 0h e 0seg
+function relogio() {
+    
+    const relogio = document.querySelector('.relogio')
+    let segundos = 0;
+    let timer
+    
+    function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos*1000)// *1000, pois o js recebe em milesegundo e assim transformamos em segundo
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'UTC'// mandando a data 01/01/1970 as 00:00h
+        })
+    } 
+    
+    function iniciaRelogio(params) {
+        timer = setInterval(function(){
+            segundos++;
+            relogio.innerHTML = criaHoraDosSegundos(segundos);
+        },1000)  
+    }
+    
+    document.addEventListener('click', function(e){
+        const el = e.target; // e.targt: retorna onde clicamos na tela
+        
+        if(el.classList.contains('iniciar')){
+            relogio.classList.remove('pausado')
+            clearInterval(timer)
+            iniciaRelogio()
+        }
+        
+        if(el.classList.contains('pausar')){
+            clearInterval(timer);
+            relogio.classList.add('pausado')
+        }
+        
+        if(el.classList.contains('zerar')){
+            relogio.classList.remove('pausado')
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00'
+            segundos = 0;
+        }
     })
+    
+    // para nao criarmos varios EventList podemos fazer como acima:
+    /*
+    const iniciar = document.querySelector('.iniciar')
+    const pausar = document.querySelector('.pausar')
+    const zerar = document.querySelector('.zerar')
+    
+    iniciar.addEventListener('click', function event(){
+        relogio.classList.remove('pausado')
+        clearInterval(timer)
+        iniciaRelogio()  
+    })
+    
+    pausar.addEventListener('click', function event(){
+        clearInterval(timer);
+        relogio.classList.add('pausado')
+    })
+    
+    zerar.addEventListener('click', function event(){
+        clearInterval(timer);
+        relogio.innerHTML  = '00:00:00'; 
+        segundos = 0;
+    })
+    
+    */
 }
 
-console.log(criaHoraDosSegundos(10*1000))// pois o js ler como milesico de segundo em vez de 10 seg
-
-
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar')
-let segundos = 0
-
-function iniciaRelogio() {
-    const timer = setInterval(function (params) {
-        segundos++;
-        relogio.innerHTML = segundos;
-    }, 1000);
-
-    iniciar.addEventListener('click', function(event){
-        iniciaRelogio();
-    })
-        
-    }
+relogio();
